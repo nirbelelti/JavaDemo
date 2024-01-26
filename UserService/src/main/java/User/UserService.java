dc5ddd329bcc81edacc29cdd717baf7ea6e19edd
nirbelelti <nirbelelti@gmail.com>
2024-01-26 10:01:37 +0100

    Remove SQLight db from repository Remove unused function

--- UserService/src/main/java/User/UserService.java
+++ UserService/src/main/java/User/UserService.java
@@ -1,8 +1,5 @@
 package User;
 
-import java.sql.*;
-
-
 public class UserService {
 
     private String firstName;
@@ -48,30 +45,4 @@ public class UserService {
         this.firstName = firstName;
     }
 
-
-    public void save() {
-        String url = "jdbc:sqlite:persondb.db";
-        String sql = "INSERT INTO person (name, lastName,address) VALUES (?, ?, ?)";
-
-        try (Connection connection = DriverManager.getConnection(url);
-             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-
-            preparedStatement.setString(1, this.firstName);
-            preparedStatement.setString(1, this.lastName);
-            preparedStatement.setString(2, this.address);
-
-            int rowsAffected = preparedStatement.executeUpdate();
-
-            if (rowsAffected > 0) {
-                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-                if (generatedKeys.next()) {
-                    this.id = generatedKeys.getInt(1);
-                }
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-    }
 }
