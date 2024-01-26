aeb9ea20ea028fae5f755abc5a15b1509de2fa0b
nirbelelti <nirbelelti@gmail.com>
2024-01-26 13:22:00 +0100

    Create post repository steps

---
+++ PostService/src/test/java/post/steps/PostRepositorySteps.java
@@ -0,0 +1,37 @@
+package post.steps;
+
+import io.cucumber.java.en.*;
+import post.Post;
+import post.PostRepository;
+
+import static org.junit.Assert.assertEquals;
+
+public class PostRepositorySteps {
+
+    int postId;
+
+    int userId = 1;
+    String title  = "My first post";
+    String content = "This is my first post";
+
+    Post post = new Post(1,userId, title, content);
+
+    @Given("I have a post with userId {int} title {string} and body {string}")
+    public void iHaveAPostWithUserIdTitleAndBody(int userId, String title, String body) {
+        assertEquals(userId, post.getUserId());
+        assertEquals(title, post.getTitle());
+        assertEquals(body, post.getBody());
+    }
+
+    @When("I create the post")
+    public void iCreateThePost() {
+       postId = PostRepository.insert(post);
+    }
+
+
+
+    @Then("the post should be saved in the repository")
+    public void thePostShouldBeSavedInTheRepository() {
+        assertEquals(1, postId);
+    }
+}
