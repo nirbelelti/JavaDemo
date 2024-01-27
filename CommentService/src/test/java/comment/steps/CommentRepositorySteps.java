f423cd872d99831ce61aae0e89cfea4f2926dd34
nirbelelti <nirbelelti@gmail.com>
2024-01-27 19:53:29 +0100

    Save comment to repository tests

---
+++ CommentService/src/test/java/comment/steps/CommentRepositorySteps.java
@@ -0,0 +1,11 @@
+package comment.steps;
+
+import io.cucumber.java.en.Given;
+
+public class CommentRepositorySteps {
+
+      @Given("a new comment object with the following attributes id  body  postId  userId")
+        public void aNewCommentObjectWithTheFollowingAttributesIdBodyPost_idUser_id() {
+          throw new io.cucumber.java.PendingException();
+    }
+}
