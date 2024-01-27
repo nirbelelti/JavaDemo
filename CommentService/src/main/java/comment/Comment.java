d99e74de57abf2571fa086d9117a5ffc586135ba
nirbelelti <nirbelelti@gmail.com>
2024-01-27 19:28:01 +0100

    Create Comment Object and tests

--- CommentService/src/main/java/comment/Comment.java
+++ CommentService/src/main/java/comment/Comment.java
@@ -1,4 +1,50 @@
 package comment;
 
 public class Comment {
+    private int id;
+    private String body;
+    private int postId;
+    private int userId;
+
+    public Comment(int id, String body, int postId, int userId) {
+        this.id = id;
+        this.body = body;
+        this.postId = postId;
+        this.userId = userId;
+    }
+
+    public Comment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
--- CommentService/src/test/java/comment/steps/CommentSteps.java
+++ CommentService/src/test/java/comment/steps/CommentSteps.java
@@ -1,23 +1,41 @@
 package comment.steps;
 
+import comment.Comment;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
 public class CommentSteps {
 
+    int id;
+    int postId;
+    int userId;
+    String body;
+
+        Comment comment = new Comment(id, body, postId, userId);
         @Given("a new comment object with the following attributes id  body  postId  userId")
         public void aNewCommentObjectWithTheFollowingAttributesIdBodyPost_idUser_id() {
-                throw new io.cucumber.java.PendingException();
+            id = 1;
+            postId = 1;
+            userId = 1;
+            body = "Comment body";
     }
 
     @When("I create the comment")
     public void iCreateTheComment() {
-        throw new io.cucumber.java.PendingException();
+        comment.setId(id);
+        comment.setBody(body);
+        comment.setPostId(postId);
+        comment.setUserId(userId);
     }
 
     @Then("the object should have the following attributes id  body  postId  userId")
     public void theObjectShouldHaveTheFollowingAttributesIdBodyPostIdUserId() {
-        throw new io.cucumber.java.PendingException();
+        assertEquals(id, comment.getId());
+        assertEquals(body, comment.getBody());
+        assertEquals(postId, comment.getPostId());
+        assertEquals(userId, comment.getUserId());
     }
 }
