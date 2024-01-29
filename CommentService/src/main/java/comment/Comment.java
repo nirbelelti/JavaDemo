e787086725a4e30b54cb076bdc623d39d1ad570b
nirbelelti <nirbelelti@gmail.com>
2024-01-29 10:43:40 +0100

    Reformat file

--- CommentService/src/main/java/comment/Comment.java
+++ CommentService/src/main/java/comment/Comment.java
@@ -13,15 +13,20 @@ public class Comment {
         this.userId = userId;
     }
 
-    public Comment() {
-    }
-
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getBody() {
         return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
     }
 
     public int getPostId() {
@@ -32,13 +37,7 @@ public class Comment {
         return userId;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
-    public void setBody(String body) {
-        this.body = body;
-    }
 
     public void setPostId(int postId) {
         this.postId = postId;
