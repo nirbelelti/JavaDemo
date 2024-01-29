f1012d647e421064a9dbd4f01fa7862e47edfe78
nirbelelti <nirbelelti@gmail.com>
2024-01-29 11:48:05 +0100

    Get all comments by userId

--- CommentService/src/main/java/comment/CommentRepository.java
+++ CommentService/src/main/java/comment/CommentRepository.java
@@ -167,4 +167,28 @@ public class CommentRepository {
         return comments;
     }
 
+    public static ArrayList<Comment> findAllByUserId(int userId) {
+        String sql = "SELECT * FROM comment WHERE user_id = ?";
+        ArrayList<Comment> comments = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url);
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, userId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String body = resultSet.getString("body");
+                int postId = resultSet.getInt("post_id");
+                comments.add(new Comment(id, body, postId, userId));
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return comments;
+    }
+
 }
