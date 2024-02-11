fbfb5e7accc34097005bf593cfb46ba8b513bf85
nirbelelti <nirbelelti@gmail.com>
2024-02-11 14:55:45 +0100

    User messages consumer

--- UserService/src/main/java/user/MsgConsumer.java
+++ UserService/src/main/java/user/MsgConsumer.java
@@ -9,11 +9,17 @@ import java.nio.charset.StandardCharsets;
 import org.rabbitmq.RabbitMQUtils;
 
 public class MsgConsumer {
-    private final static String QUEUE_NAME = "createUserQueue";
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static UserFacade facade = new UserFacade();
+    private final static String CREATE_USER_QUEUE = "createUserQueue";
+    private final static String GET_USER_QUEUE = "getUserQueue";
+    private final static String GET_ALL_USERS_QUEUE = "getAllUsersQueue";
+    private final static String UPDATE_USER_QUEUE = "updateUserQueue";
+    private final static String DELETE_USER_QUEUE = "deleteUserQueue";
 
-    public static void main(String[] args) {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final UserFacade facade = new UserFacade();
+
+
+    public void createUserStart() {
         try {
             // Establish a connection to RabbitMQ server
             Connection connection = RabbitMQUtils.getConnection();
@@ -22,7 +28,7 @@ public class MsgConsumer {
             Channel channel = connection.createChannel();
 
             // Declare a queue (creates the queue if not exists)
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueDeclare(CREATE_USER_QUEUE, false, false, false, null);
 
             // Create a consumer and specify the callback to handle incoming messages
             DeliverCallback deliverCallback = (consumerTag, delivery) -> {
@@ -51,12 +57,153 @@ public class MsgConsumer {
             };
 
             // Consume messages from the queue
-            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+            channel.basicConsume(CREATE_USER_QUEUE, true, deliverCallback, consumerTag -> {
+            });
 
             // Keep the consumer running (you can add your own logic to gracefully stop the consumer)
             System.out.println(" [*] Waiting for messages. To exit press Ctrl+C");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void getAllUsersStart() {
+        try {
+            Connection connection = RabbitMQUtils.getConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(GET_ALL_USERS_QUEUE, false, false, false, null);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String users = String.valueOf(facade.allUsers());
+
+                AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), properties, users.getBytes(StandardCharsets.UTF_8));
+            };
+            channel.basicConsume(GET_ALL_USERS_QUEUE, true, deliverCallback, consumerTag -> {
+            });
+
+            System.out.println(" [*] Waiting for messages. To exit press Ctrl+C");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getUserStart() {
+        try {
+            Connection connection = RabbitMQUtils.getConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(GET_USER_QUEUE, false, false, false, null);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+
+                JsonNode jsonNode = objectMapper.readTree(message);
+                int id = jsonNode.get("id").asInt();
+
+                String user = facade.getUser(id);
+
+                AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), properties, user.getBytes(StandardCharsets.UTF_8));
+            };
+
+            channel.basicConsume(GET_USER_QUEUE, true, deliverCallback, consumerTag -> {
+            });
+
+            System.out.println(" [*] Waiting for messages...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateUserStart() {
+        try {
+            Connection connection = RabbitMQUtils.getConnection();
+
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(UPDATE_USER_QUEUE, false, false, false, null);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+                JsonNode jsonNode = objectMapper.readTree(message);
+                int id = jsonNode.get("id").asInt();
+                String firstName = jsonNode.get("firstName").asText();
+                String lastName = jsonNode.get("lastName").asText();
+                String address = jsonNode.get("address").asText();
+
+                // Process the data
+                boolean updated = facade.updateUser(id, firstName, lastName, address);
+                String responseMessage = updated ? "User updated" : "User not found";
+
+                // Send the response back to the sender using the replyTo and correlationId
+                AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), properties, responseMessage.getBytes(StandardCharsets.UTF_8));
+
+                System.out.println(" [x] Response sent: '" + responseMessage + "'");
+            };
+
+            channel.basicConsume(UPDATE_USER_QUEUE, true, deliverCallback, consumerTag -> {
+            });
+
+            System.out.println(" [*] Waiting for messages...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUserStart() {
+        try {
+            Connection connection = RabbitMQUtils.getConnection();
+
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(DELETE_USER_QUEUE, false, false, false, null);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                System.out.println(" [x] Received '" + message + "'");
+
+                JsonNode jsonNode = objectMapper.readTree(message);
+                int id = Integer.valueOf(message);
+
+                boolean deleted = facade.deleteUser(id);
+                String responseMessage = deleted ? "User deleted" : "User not found";
+
+                AMQP.BasicProperties properties = new AMQP.BasicProperties.Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), properties, responseMessage.getBytes(StandardCharsets.UTF_8));
+
+                System.out.println(" [x] Response sent: '" + responseMessage + "'");
+            };
+
+            channel.basicConsume(DELETE_USER_QUEUE, true, deliverCallback, consumerTag -> {
+            });
+            System.out.println(" [*] Waiting for messages....");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MsgConsumer consumer = new MsgConsumer();
+        consumer.createUserStart();
+        consumer.getUserStart();
+        consumer.getAllUsersStart();
+        consumer.updateUserStart();
+        consumer.deleteUserStart();
     }
 }
